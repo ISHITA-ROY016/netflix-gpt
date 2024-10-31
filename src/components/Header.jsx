04b203f256fa1f72adee5e4e3bf9991919488508
ISHITA ROY <royishita016@gmail.com>
2024-10-31 18:48:37 +0530

    Sign-up/sign-in page built

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import Logo from "../assets/logo.svg";
 
 const Header = () => {
   return (
-    <div>
-      
+    <div className="absolute px-8 py-2 bg-gradient-to-b from-black z-10">
+      <img src={Logo} alt="logo" className="w-32" />
     </div>
   )
 }
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,11 +1,56 @@
-import React from 'react'
-
+import Header from "./Header";
+import Background from "../assets/bg-img.jpg";
+import { useState } from "react";
 const Login = () => {
+  const [isSignedInForm, setIsSignedInForm] = useState(true);
+  const toggleSignInForm = () => {
+    setIsSignedInForm(!isSignedInForm);
+  };
   return (
-    <div>
-      Login
-    </div>
-  )
-}
+    <>
+      <Header />
+      <img src={Background} alt="" className="object-cover w-screen h-screen" />
+      <form
+        action=""
+        className="absolute flex flex-col w-96 bg-black bg-opacity-85 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white p-16"
+      >
+        <h1 className="font-bold text-3xl mb-8">
+          {isSignedInForm ? "Sign In" : "Sign Up"}
+        </h1>
+        {!isSignedInForm && (
+          <input
+            type="text"
+            placeholder="Full Name"
+            className="p-3 my-3 bg-transparent outline-1 outline-white outline"
+          />
+        )}
+        <input
+          type="email"
+          placeholder="Email or mobile number"
+          className="p-3 my-3 bg-transparent outline-1 outline-white outline"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className="p-3 my-3 bg-transparent outline-1 outline-white outline"
+        />
+        <button className="py-2 my-4 bg-red-700 text-xl font-semibold rounded-md">
+          {isSignedInForm ? "Sign In" : "Sign Up"}
+        </button>
+        {isSignedInForm ? (
+          <h3 className="text-gray-300">
+            New to Netflix? <button onClick={toggleSignInForm} className="text-white font-bold">Sign up</button>{" "}
+            now.
+          </h3>
+        ) : (
+          <h3 className="text-gray-300">
+            Already Registered?{" "}
+            <button onClick={toggleSignInForm} className="text-white font-bold">Sign in</button> now.
+          </h3>
+        )}
+      </form>
+    </>
+  );
+};
 
 export default Login;

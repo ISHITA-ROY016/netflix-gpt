2effd149e7fda3666c12a476128c6dc54b470bb5
ISHITA ROY <royishita016@gmail.com>
2024-11-12 18:46:51 +0530

    secured api keys

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,7 +2,7 @@ export const NOW_PLAYING_API_OPTIONS = {
     method: 'GET',
     headers: {
         // 'x-rapidapi-key': '0bd6ee3cbdmsh4bbd37d9e53dd20p1509b9jsn86f3fd4488be',
-        'x-rapidapi-key': '5aaf5741acmsh4bbd6a18c164446p112a9fjsncee261249678',
+        'x-rapidapi-key': process.env.REACT_APP_STREAM_AVAILABILITY,
         'x-rapidapi-host': 'streaming-availability.p.rapidapi.com'
     },
 };
@@ -10,16 +10,16 @@ export const NOW_PLAYING_API_OPTIONS = {
 export const MOVIES_API_OPTIONS = {
     method: 'GET',
     headers: {
-        'x-rapidapi-key': '0bd6ee3cbdmsh4bbd37d9e53dd20p1509b9jsn86f3fd4488be',
+        'x-rapidapi-key': process.env.REACT_APP_MOVIESVERSE,
         'x-rapidapi-host': 'moviesverse1.p.rapidapi.com'
     }
 };
 export const TRAILER_API_OPTIONS = {
     method: 'GET',
     headers: {
-        'x-rapidapi-key': '0bd6ee3cbdmsh4bbd37d9e53dd20p1509b9jsn86f3fd4488be',
+        'x-rapidapi-key': process.env.REACT_APP_OPENAI_KEY_IMDB146,
         'x-rapidapi-host': 'imdb146.p.rapidapi.com'
     }
 };
 
-export const OPENAI_KEY="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";
+export const OPENAI_KEY=process.env.REACT_APP_OPENAI_KEY;

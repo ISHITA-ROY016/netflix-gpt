652833608935a143d82aa966458d18bffa7fafb2
ISHITA ROY <royishita016@gmail.com>
2024-11-12 18:49:50 +0530

    added api key

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,7 @@
 export const NOW_PLAYING_API_OPTIONS = {
     method: 'GET',
     headers: {
-        // 'x-rapidapi-key': '0bd6ee3cbdmsh4bbd37d9e53dd20p1509b9jsn86f3fd4488be',
+        // 'x-rapidapi-key': REACT_APP_STREAM_AVAILABILITY2,
         'x-rapidapi-key': process.env.REACT_APP_STREAM_AVAILABILITY,
         'x-rapidapi-host': 'streaming-availability.p.rapidapi.com'
     },
@@ -22,4 +22,4 @@ export const TRAILER_API_OPTIONS = {
     }
 };
 
-export const OPENAI_KEY=process.env.REACT_APP_OPENAI_KEY;
+export const OPENAI_KEY = process.env.REACT_APP_OPENAI_KEY;

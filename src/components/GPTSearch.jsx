e5afa8890529248e538efffab2ec522196de2eb2
ISHITA ROY <royishita016@gmail.com>
2024-11-13 23:51:37 +0530

    scrollbar added

--- src/components/GPTSearch.jsx
+++ src/components/GPTSearch.jsx
@@ -6,12 +6,12 @@ import Background from "../assets/bg-img.jpg";
 const GPTSearch = () => {
   return (
     <div
-      className="relative min-h-screen w-full bg-cover"
+      className="relative min-h-screen w-full bg-cover opacity-80"
       style={{
         backgroundImage: `url(${Background})`,
       }}
     >
-      <div className="absolute inset-0 bg-black opacity-70 -z-10"></div>
+      <div className="absolute inset-0 bg-black opacity-5 -z-10"></div>
       <GPTSearchBar />
       <GPTMovieSuggestions />
     </div>
